ec1e5d0b03e8c3854fed50bf53eea85c128472be
Michele Zamuner <michelezamuner@gmail.com>
2020-12-29 10:58:16 +0100

    Controls tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+/// <reference types='leaflet-sidebar-v2' />
 
-import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LeafletModule} from '@asymmetrik/ngx-leaflet';
+import { OsmMapComponent} from "./osm-map/osm-map.component";
+import { AppComponent } from './app.component';
+import { NgxLeafletFullscreenModule } from '@runette/ngx-leaflet-fullscreen';
+import { NgxLoadingControlModule } from '@runette/ngx-leaflet-loading';
+import { NgxLeafletLocateModule } from '@runette/ngx-leaflet-locate';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { NgxSidebarControlModule } from '@runette/ngx-leaflet-sidebar';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
     FormsModule,
-    LeafletModule
+    ReactiveFormsModule,
+    LeafletModule,
+    NgxLeafletFullscreenModule,
+    NgxLoadingControlModule,
+    NgxLeafletLocateModule,
+    MatSlideToggleModule,
+    MatGridListModule,
+    NgxSidebarControlModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [ AppComponent, OsmMapComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
